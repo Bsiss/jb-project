57432181b319d3d04a68d2f8c0fd2765ddc622b2
Brandon <Brandon Sissenah>
2020-03-31 18:59:12 -0400

    added a thank you button at end of atricle

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function moveScrollIndicator() {
     const percentage = ((window.scrollY) / maxScrollableHeight) * 100;
     scrollIndicatorElt.style.width = percentage + '%';
 }
+
+//Nav minimize on scroll, give users more room to read article and makes sections clear while at top.
+window.onscroll = function() { scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav").style.fontSize = "20px";
+    } else {
+        document.getElementById("nav").style.fontSize = "40px";
+    }
+}
+
+//Thank you message button at end of quiz & article, reveals thank you message to reader.
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+
