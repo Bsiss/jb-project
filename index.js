6b9c0d0219419aed4c605954bb95b28940ed5176
Brandon <Brandon Sissenah>
2020-04-02 00:33:10 -0400

    adding to back to top button function JS

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+topButton = document.getElementById("top")
+
+
 document.querySelector('.tothetop').addEventListener('click', event => {
     window.scrollTo({left:0, top:0, behavior:"smooth"})
 }) // Back to top button
